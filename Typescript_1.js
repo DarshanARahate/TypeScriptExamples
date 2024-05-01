cf66f30afc11dda0017725a8913ef3bbf2855d52
darshanrahate <rahate.darshan@gmail.com>
2024-05-01 06:20:29 +0530

    First Commit

---
+++ Typescript_1.js
@@ -0,0 +1,10 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = 'TypeScript';
+console.log(greeter(user));
+function addNumbers(a, b) {
+    return a + b;
+}
+var sum = addNumbers("Typescript", 25);
+console.log("Sum of the numbers is " + sum);
---
+++ Typescript_1.ts
@@ -0,0 +1,14 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = 'TypeScript';
+
+console.log(greeter(user));
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+var sum = addNumbers("Typescript", 25);
+console.log("Sum of the numbers is " + sum)
